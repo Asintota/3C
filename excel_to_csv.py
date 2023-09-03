d6ec1fa4c2582a18e89a8c6def0874ff911967f3
Asintota <81699016+Asintota@users.noreply.github.com>
2023-09-03 13:47:14 +0200

    af

--- excel_to_csv.py
+++ excel_to_csv.py
@@ -1,25 +1,38 @@
 import pandas as pd
 
 # Especifica la ruta del archivo XLSX
-archivo_excel = 'C:/Users/pablo/OneDrive/Github/Progreso/tareas.xlsx'
-csv_file = 'C:/Users/pablo/OneDrive/Github/Progreso/progreso.csv'
+actitud_excel = 'C:/Users/pablo/OneDrive/Github/Progreso/actitud.xlsx'
+csv_file = 'C:/Users/pablo/OneDrive/Github/Progreso/datos.csv'
 # Lee el archivo XLSX en un DataFrame de pandas
-df_excel = pd.read_excel(archivo_excel)
+
+Astronauta = pd.read_excel(actitud_excel, sheet_name = "Astronauta")
+Senador = pd.read_excel(actitud_excel, sheet_name = "Senador")
+Mago = pd.read_excel(actitud_excel, sheet_name = "Mago")
+Guerrero = pd.read_excel(actitud_excel, sheet_name = "Guerrero")
+
+
+
 
 # Extract the "Resultados" column as a separate Series
-resultados_column = df_excel['Resultados']
+Astronauta_resultados = Astronauta['Resultados']
+Senador_resultados = Senador['Resultados']
+Mago_resultados = Mago['Resultados']
+Guerrero_resultados = Guerrero['Resultados']
 
 # Read the CSV file into a DataFrame
 df_csv = pd.read_csv(csv_file)
 
 # Update the "Tareas" column in the CSV DataFrame with the "Resultados" data
-df_csv['Tareas'] = resultados_column
+df_csv['Astronauta'] = Astronauta_resultados
+df_csv['Senador'] = Senador_resultados
+df_csv['Mago'] = Mago_resultados
+df_csv['Guerrero'] = Guerrero_resultados
 
 # Write the updated DataFrame back to the CSV file
 df_csv.to_csv(csv_file, index=False)
 
 
-print("Data from 'Resultados' column in Excel has been copied to the 'Tareas' column in the CSV file.")
+print("Work done")
 
 
 
