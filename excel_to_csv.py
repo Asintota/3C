d40d1d5ff7f2d20082a8591903fb1b3c3709a625
Asintota <81699016+Asintota@users.noreply.github.com>
2023-09-07 14:53:07 +0200

    g

--- excel_to_csv.py
+++ excel_to_csv.py
@@ -2,14 +2,27 @@ import pandas as pd
 
 # Especifica la ruta del archivo XLSX
 actitud_excel = 'C:/Users/pablo/OneDrive/Github/Progreso/actitud.xlsx'
-csv_file = 'C:/Users/pablo/OneDrive/Github/Progreso/datos.csv'
+csv_file = 'C:/Users/pablo/OneDrive/Github/Progreso/csv/datos.csv'
+csv_astronauta = 'C:/Users/pablo/OneDrive/Github/Progreso/csv/Astronauta.csv'
+csv_senador = 'C:/Users/pablo/OneDrive/Github/Progreso/csv/Senador.csv'
+csv_ninja = 'C:/Users/pablo/OneDrive/Github/Progreso/csv/Ninja.csv'
+csv_mago = 'C:/Users/pablo/OneDrive/Github/Progreso/csv/Mago.csv'
+
 # Lee el archivo XLSX en un DataFrame de pandas
 
 Astronauta = pd.read_excel(actitud_excel, sheet_name = "Astronauta")
 Senador = pd.read_excel(actitud_excel, sheet_name = "Senador")
 Mago = pd.read_excel(actitud_excel, sheet_name = "Mago")
-Guerrero = pd.read_excel(actitud_excel, sheet_name = "Guerrero")
+Ninja = pd.read_excel(actitud_excel, sheet_name = "Ninja")
 
+Astronauta.to_csv(csv_astronauta, index=False)
+print(f"Excel file has been converted to CSV file 'Astronauta.csv'.")
+Senador.to_csv(csv_senador, index=False)
+print(f"Excel file has been converted to CSV file 'Senador.csv'.")
+Mago.to_csv(csv_mago, index=False)
+print(f"Excel file has been converted to CSV file 'Mago.csv'.")
+Ninja.to_csv(csv_ninja, index=False)
+print(f"Excel file has been converted to CSV file 'Ninja.csv'.")
 
 
 
@@ -17,7 +30,10 @@ Guerrero = pd.read_excel(actitud_excel, sheet_name = "Guerrero")
 Astronauta_resultados = Astronauta['Resultados']
 Senador_resultados = Senador['Resultados']
 Mago_resultados = Mago['Resultados']
-Guerrero_resultados = Guerrero['Resultados']
+Ninja_resultados = Ninja['Resultados']
+
+
+#
 
 # Read the CSV file into a DataFrame
 df_csv = pd.read_csv(csv_file)
@@ -26,11 +42,11 @@ df_csv = pd.read_csv(csv_file)
 df_csv['Astronauta'] = Astronauta_resultados
 df_csv['Senador'] = Senador_resultados
 df_csv['Mago'] = Mago_resultados
-df_csv['Guerrero'] = Guerrero_resultados
+df_csv['Ninja'] = Ninja_resultados
 
 # Write the updated DataFrame back to the CSV file
 df_csv.to_csv(csv_file, index=False)
-
+print(f"Excel file has been converted to CSV file 'datos.csv'.")
 
 print("Work done")
 
