508da12e2d441282e8c934d62e37e3d4e905a9f4
Asintota <81699016+Asintota@users.noreply.github.com>
2023-09-17 13:41:58 +0200

    Tu mensaje de commit aquí

---
+++ github_push.py
@@ -0,0 +1,51 @@
+import os
+import git
+from github import Github
+
+# Ruta del directorio del repositorio local
+repo_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Mensaje del commit
+commit_message = 'Tu mensaje de commit aquí'
+
+# Nombre de la rama en la que deseas hacer el commit y el push
+branch_name = 'main'
+
+# Token de autenticación de GitHub (necesario para realizar el push)
+github_token = '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'
+
+# Nombre de usuario de GitHub
+github_username = 'Asintota'
+
+# Nombre del repositorio en GitHub
+github_repo_name = '3C'
+
+# Ruta al archivo remoto del repositorio en GitHub
+remote_repo_url = f'https://github.com/{github_username}/{github_repo_name}.git'
+
+# Cambia al directorio del repositorio local
+os.chdir(repo_dir)
+
+# Inicializa el repositorio Git
+repo = git.Repo(repo_dir)
+
+# Agrega todos los cambios al área de preparación
+repo.git.add('--all')
+
+# Realiza el commit
+repo.git.commit('-m', commit_message)
+
+# Conecta con GitHub usando tu token
+g = Github(github_token)
+
+# Obtiene el repositorio remoto en GitHub
+repo_github = g.get_repo(f'{github_username}/{github_repo_name}')
+
+# Obtiene la rama específica en la que deseas hacer el push
+branch = repo_github.get_branch(branch_name)
+
+# Sube los cambios al repositorio remoto en la rama especificada
+repo.git.push(remote_repo_url, branch_name)
+
+print(f'Commit y push exitosos en la rama {branch_name} de {github_repo_name}.')
+
